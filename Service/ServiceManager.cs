fb41bb2a54b13dd785b4bd7b1646c1720d5e6d21
Sergey Osokin <osokin-si@yandex.ru>
2023-01-23 22:53:25 +0400

    Add service layer

---
+++ Service/ServiceManager.cs
@@ -0,0 +1,20 @@
+﻿using Contracts;
+using Service.Contracts;
+
+namespace Service
+{
+    public sealed class ServiceManager : IServiceManager
+    {
+        private readonly Lazy<ICompanyService> _companyService;
+        private readonly Lazy<IEmployeeService> _employeeService;
+
+        public ServiceManager(IRepositoryManager repositoryManager, ILoggerManager logger)
+        {
+            _companyService = new Lazy<ICompanyService>(() =>
+                new CompanyService(repositoryManager, logger));
+        }
+        public ICompanyService CompanyService => throw new NotImplementedException();
+
+        public IEmployeeService EmployeeService => throw new NotImplementedException();
+    }
+}
